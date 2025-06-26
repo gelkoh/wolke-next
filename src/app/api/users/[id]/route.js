1c71c8d39a5f7ccdd8e974d2a4262f67882cefb3
Gelkoh <ole_kirchner@gmx.de>
2025-06-26 13:31:15 +0200

    Add endpoint for getting a user

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,51 @@
+const mongodbURI = 'mongodb://backend:27017/wolke-next';
+import mongoose from "mongoose"
+import { UserSchema } from "../../../../../model/schema.js"
+
+let isConnected = false
+
+async function connectDb() {
+    if (isConnected) {
+        console.log("Using existing database connection") 
+        return;
+    }
+
+    try {
+        await mongoose.connect(mongodbURI)
+        isConnected = true
+        console.log("MongoDB connected successfully")
+    } catch(error) {
+        console.error("MongoDB connection error: ", error)
+        throw new Error("Failed to connect to database")
+    }
+}
+
+export async function GET(request, { params }) {
+    await connectDb() 
+
+    const { id } = params
+
+    const User = mongoose.model("User", UserSchema)
+
+    try {
+        const user = await User.findOne({ id: id })
+
+        if (!user) {
+            return new Response(JSON.stringify({ message: "User not found" }), {
+                status: 404,
+                headers: { "Content-type": "application/json" }
+            })
+        }
+
+        return new Response(JSON.stringify(user), {
+            status: 200,
+            headers: { "Content-type": "application/json" }
+        })
+    } catch(error) {
+        console.error("Error fetching user: ", error)
+        return new Response(JSON.stringify({ message: "Internal Server Error", error: error.message }), {
+            status: 500,
+            headers: { "Content-type": "application/json"}
+        })
+    }
+}
